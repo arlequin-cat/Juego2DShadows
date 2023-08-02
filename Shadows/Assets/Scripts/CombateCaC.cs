ad56ed861b77df89306430793ff280c79e1398d4
arlequin-cat <alexmurcia2000@gmail.com>
2023-08-01 23:32:59 -0500

    se realizaron los ajustes a las UI, se implemetaron codigos para que el enemigo muera y un codigo de ataque para el jugador

---
+++ Shadows/Assets/Scripts/CombateCaC.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombateCaC : MonoBehaviour
+{
+    [SerializeField] private Transform controladorGolpe;
+    [SerializeField] private float radioGolpe;
+    [SerializeField] private float dañoGolpe;
+    [SerializeField] private float tiempoEntreAtaques;
+    [SerializeField] private float tiempoSiguienteAtaque;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (tiempoSiguienteAtaque > 0)
+        {
+            tiempoSiguienteAtaque -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire1") && tiempoSiguienteAtaque <= 0)
+        {
+            Golpe();
+            tiempoSiguienteAtaque = tiempoEntreAtaques;
+        }
+    }
+
+    private void Golpe()
+    {
+        animator.SetTrigger("Ataque");
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
+        foreach (Collider2D colisionador in objetos)
+        {
+            if (colisionador.CompareTag("Enemigo"))
+            {
+                colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
+}
